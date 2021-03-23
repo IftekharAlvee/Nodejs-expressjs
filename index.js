35f557c1b4fd404570d149d1a87a0f83a2cc282a
Iftekhar Alvee <alveee1998@gmail.com>
2021-03-23 21:48:13 +0600

    banana

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send("thanks for calling me");
+    const bepari = {
+        name: "Ada",
+        Price: 220
+    }
+    res.send(bepari);
 });
+app.get('/products/banana', (req,res) => {
+    const banana = {
+        name: "banana",
+        price: 65,
+        quantinity: "10 dozen"}
+    res.send(banana);
+})
 
-app.listen(3000, () => console.log("Listening to port 3000") )
+app.listen(3000, () => console.log("Listening to port 3000") );
