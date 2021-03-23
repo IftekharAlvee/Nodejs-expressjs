4f8d9764fe7784fdd2ab5fa52442be23346443ee
Iftekhar Alvee <alveee1998@gmail.com>
2021-03-24 00:58:16 +0600

    postman

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 const users = ['Asad','Emon','Remon','Alvee','Ashiq','Tausif'];
 
@@ -23,6 +30,12 @@ app.get('/user/:id',(req,res) => {
     const id = req.params.id;
     const name = users[id];
     res.send({id:id, name:name});
+})
+// post
+app.post('/newUser',(req,res) => {
+    const user = req.body;
+    user.Id = 55;
+    res.send(user)
 })
 
 app.listen(3000, () => console.log("Listening to port 3000") );
